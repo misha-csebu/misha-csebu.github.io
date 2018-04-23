ba92614e274b15a819ebaec21d1268069cce604d
Harshit Singhai <30886142+harshitsinghai77@users.noreply.github.com>
2018-04-24 01:14:06 +0530

    Add files via upload

---
+++ content/misha/js/doraemon.js
@@ -0,0 +1,30 @@
+var result = [];
+
+$(window).keydown(function(event){
+	var a = event.keyCode;
+	console.log(a);
+	result.push(a);
+	print();
+	
+});
+
+
+function print(){
+		var something = 0;
+		result.forEach(function(shadow){
+			something += shadow;
+		});
+		if(something === 666){
+			onSuccess();
+		}
+};
+
+
+function onSuccess(){
+	$("#video1").attr("src","video.mp4");
+	$("#video2").attr("src","video.mp4");
+	$("#myvideo")[0].load();
+	$("#myvideo")[0].play();
+}
+
+
